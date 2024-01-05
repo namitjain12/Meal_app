172d5d42bdeabe8fee46203a3f2bbb82b9520292
namitjain12 <142582235+namitjain12@users.noreply.github.com>
2024-01-05 15:12:56 +0530

    first commit

---
+++ meal_app/lib/models/category.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Category{
+ const Category({
+  required this.id,
+  required this.title,
+  this.color = Colors.orange,
+ });
+
+
+  final String id;
+  final Color color;
+  final String title;
+}
